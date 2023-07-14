6865e8f155ae96f5ae0f528f51048009ff9cb6e2
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-07-14 17:29:39 -0600

    schema added to prop

---
+++ ListFourteeners/src/main/java/com/skilldistillery/fourteeners/ListFourteenersApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fourteeners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListFourteenersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListFourteenersApplication.class, args);
+	}
+
+}
---
+++ ListFourteeners/src/test/java/com/skilldistillery/fourteeners/ListFourteenersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fourteeners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListFourteenersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
